9293be72aa28adb5080155de6a7da29a5686cd3b
Dan Stocker <dan@kwaia.com>
2012-03-21 17:19:07 +0100

    .set and .shallow added to utils

--- js/utils.js
+++ js/utils.js
@@ -23,6 +23,41 @@ var prime = prime || {};
                 }
             }
             return result;
+        },
+
+        /**
+         * Sets a value on the path specified by argument list.
+         * Last param is the value.
+         * @param object {object} Root object.
+         */
+        set: function (object) {
+            var tmp = object,
+                length = arguments.length - 2,
+                i, key;
+            for (i = 1; i < length; i++) {
+                key = arguments[i];
+                if (typeof tmp[key] !== 'object') {
+                    tmp[key] = {};
+                }
+                tmp = tmp[key];
+            }
+            tmp[arguments[i]] = arguments[i+1];
+        },
+
+        /**
+         * Creates shallow copy of object.
+         * @param object {object} Object to be copied.
+         * @returns {object} Object copy.
+         */
+        shallow: function (object) {
+            var result = {},
+                key;
+            for (key in object) {
+                if (object.hasOwnProperty(key)) {
+                    result[key] = object[key];
+                }
+            }
+            return result;
         }
     };
 }());
--- js/utils.test.js
+++ js/utils.test.js
@@ -1,13 +1,46 @@
 /*global module, test, ok, equal, notEqual, deepEqual, raises */
-(function () {
+(function ($utils) {
     module("Utils");
 
     test("Objects", function () {
+        var tmp;
+
         deepEqual(
-            prime.utils.keys({foo: "bar", hey: "ho"}),
+            $utils.keys({foo: "bar", hey: "ho"}),
             ['foo', 'hey'],
             "Obtaining keys of object"
         );
+
+        tmp = {};
+        $utils.set(tmp, 'hello', 'dolly');
+        deepEqual(
+            tmp,
+            {hello: 'dolly'},
+            "Setting string value on non-existing path"
+        );
+
+        $utils.set(tmp, 'hello', 'my', {dear: 'dolly'});
+        deepEqual(
+            tmp,
+            {
+                hello: {
+                    my: {
+                        dear: 'dolly'
+                    }
+                }
+            },
+            "Setting object value on non-existing path"
+        );
     });
-}());
+
+    test("Copying", function () {
+        var src = {hello: 'dolly'},
+            dst = $utils.shallow(src);
+        notEqual(src, dst, "Shallow copy is a different object");
+        deepEqual(src, dst, "Shallow copy has identical contents");
+        equal(src['hello'], dst['hello'], "Shallow copy property references match");
+    });
+}(
+    prime.utils
+));
 
