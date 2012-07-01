60f09226e3e7205cd43c2db18d4d79e503a9d40f
Dan Stocker <dan@kwaia.com>
2012-07-01 16:18:19 +0200

    code clarity improved in dictionary example

--- examples/dictionary/dictionary.js
+++ examples/dictionary/dictionary.js
@@ -1,6 +1,6 @@
 /*global prime, console */
 var dictionary = dictionary || {};
-(function ($) {
+(function (node) {
     dictionary.statistics = function () {
         var data = dictionary.data,
             result = {},
@@ -8,22 +8,22 @@ var dictionary = dictionary || {};
 
         // filling nodes
         for (i = 0; i < data.length; i++) {
-            $(data[i]);
+            node(data[i]);
         }
 
         // establishing connections
-        $('table')
-            .addPeers($('cup')
-                .addPeers($('small'))
-                .addPeers($('white')))
-            .addPeers($('cloth'))
-            .addPeers($('food')
-                .addPeers($('warm'))
-                .addPeers($('taste')));
+        node('table')
+            .addPeers(node('cup')
+                .addPeers(node('small'))
+                .addPeers(node('white')))
+            .addPeers(node('cloth'))
+            .addPeers(node('food')
+                .addPeers(node('warm'))
+                .addPeers(node('taste')));
 
         // traversing connections, gathering statistics
         for (i = 0; i < 1000; i++) {
-            load = $('table').hop().load;
+            load = node('table').hop().load;
             result[load] = (result[load] || 0) + 1;
         }
 
