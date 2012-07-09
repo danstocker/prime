cd3a05f9c04a2ec5ec20e55349d5736e2a8784f7
Dan Stocker <dan@kwaia.com>
2012-07-10 01:09:28 +0200

    prime.node shorthand placed in separate promise

--- js/Node.js
+++ js/Node.js
@@ -144,25 +144,26 @@ troop.promise(prime, 'Node', function (ns, className, $Peers) {
             }
         });
 
-    troop.properties.addMethod.call(prime, {
-        /**
-         * Retrieves a node from the graph or creates a new one.
-         * @param load {string} Node load.
-         * @return {prime.Node}
-         */
-        node: function (load) {
-            // shortcuts and local variable
-            var nodes = self.graph.nodes;
-
-            if (nodes.hasOwnProperty(load)) {
-                // node exists in lookup, fetching
-                return nodes[load];
-            } else {
-                // new load, creating node
-                return self.create(load);
-            }
-        }
-    });
-
     return self;
 }, prime.Peers);
+
+troop.promise(prime, 'node', function () {
+    /**
+     * Retrieves a node from the graph or creates a new one.
+     * @param load {string} Node load.
+     * @return {prime.Node}
+     */
+    return function (load) {
+        // shortcuts and local variable
+        var Node = prime.Node,
+            nodes = Node.graph.nodes;
+
+        if (nodes.hasOwnProperty(load)) {
+            // node exists in lookup, fetching
+            return nodes[load];
+        } else {
+            // new load, creating node
+            return Node.create(load);
+        }
+    };
+});
