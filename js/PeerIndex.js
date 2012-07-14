be8ec691deda8aab770f25214f602382a5886d38
Dan Stocker <dan@kwaia.com>
2012-07-14 12:13:18 +0200

    basic querying (incomplete)

--- js/PeerIndex.js
+++ js/PeerIndex.js
@@ -1,3 +1,9 @@
+/**
+ * Weight Index
+ *
+ * Index of weighted slots. Each slot in the index carries a weight,
+ * serving as a basis for random retrieval.
+ */
 /*global prime, troop */
 troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
     /**
@@ -9,17 +15,18 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
             init: function () {
                 /**
                  * List of peers in order identical to _index.
-                 * @type {string[]}
-                 * @private
-                 */
-                this._weights = [];
-
-                /**
-                 * Sorted index of weights in order of total weight.
                  * @type {number[]}
                  * @private
                  */
-                this._totals = [];
+                this._weights = [0];
+
+                /**
+                 * Sorted index of total weights.
+                 * Total weight is the cumulative weight of all slots
+                 * @type {number[]}
+                 * @private
+                 */
+                this._totals = [0];
 
                 /**
                  * Lookup for empty index spots indexed by weight first, then by position in _peers.
@@ -27,8 +34,7 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
                  * @private
                  */
                 this._empties = {};
-
-                this.lastTotal = 0;
+                this.emptyCount = 0;
             }
         }).addPrivateMethod({
             /**
@@ -65,6 +71,15 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
             }
         }).addMethod({
             /**
+             * Retrieves highest total weight from index.
+             * @return {number}
+             */
+            lastTotal: function () {
+                var totals = this._totals;
+                return totals[totals.length - 1] || 0;
+            },
+
+            /**
              * Adds index entry.
              * @param weight {number} Entry weight.
              * @return {number} Position of (new) entry in the index.
@@ -72,6 +87,7 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
             add: function (weight) {
                 var empties = this._empties,
                     totals = this._totals,
+                    total, // next total weight value in _totals
                     pos; // position of the new entry (in _weights and _totals)
 
                 if (empties.hasOwnProperty(weight)) {
@@ -83,16 +99,15 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
                     if ($utils.isEmpty(empties[weight])) {
                         // all empty slots for `weight` used up
                         delete empties[weight];
+                        this.emptyCount--;
                     }
                 } else {
                     // no empty spot available
-                    // increasing last total
-                    this.lastTotal += weight;
-
+                    total = this.lastTotal() + weight;
                     pos = totals.length;
 
                     // adding new entry to index
-                    totals.push(this.lastTotal);
+                    totals.push(total);
                     this._weights.push(weight);
                 }
 
@@ -112,6 +127,23 @@ troop.promise(prime, 'PeerIndex', function (ns, className, $utils) {
                 }
 
                 empties[weight][pos] = true;
+                this.emptyCount++;
+            },
+
+            /**
+             * Retrieves a slot based on its total weight.
+             * @param total {number} Number between zero and this.lastTotal
+             */
+            get: function (total) {
+                return this._bsearch(total);
+            },
+
+            /**
+             * Retrieves a random slot based on total weights.
+             */
+            random: function () {
+                var total = Math.random() * this.lastTotal();
+                return this._bsearch(total);
             }
         });
 
--- js/PeerIndex.test.js
+++ js/PeerIndex.test.js
@@ -1,63 +1,80 @@
 /*global prime, mocks, module, test, ok, equal, notEqual, deepEqual, raises */
-(function ($PeerIndex) {
-    module("PeerIndex");
+(function ($Index) {
+    module("Index");
 
     test("Bsearch", function () {
-        var peerIndex = $PeerIndex.create();
-        peerIndex._totals = [1, 3, 5, 6, 9];
+        var index = $Index.create();
+        index._totals = [0, 1, 3, 5, 6, 9];
 
-        equal(peerIndex._bsearch(4), 1, "Position of 4 (nearest hit)");
-        equal(peerIndex._bsearch(6), 3, "Position of 6 (exact hit)");
-        equal(peerIndex._bsearch(1), 0, "Position of 1 (low extreme)");
-        equal(peerIndex._bsearch(9), 4, "Position of 9 (high extreme)");
+        equal(index._bsearch(4), 2, "Position of 4 (nearest hit)");
+        equal(index._bsearch(6), 4, "Position of 6 (exact hit)");
+        equal(index._bsearch(0), 0, "Position of 1 (low extreme)");
+        equal(index._bsearch(9), 5, "Position of 9 (high extreme)");
     });
 
     test("Addition", function () {
-        var peerIndex = $PeerIndex.create(),
+        var index = $Index.create(),
             tmp;
 
-        tmp = peerIndex.add(5);
-        equal(tmp, 0, "Index position");
-        deepEqual(peerIndex._weights, [5], "Weights after adding 5");
-        deepEqual(peerIndex._totals, [5], "Totals after adding 5");
+        equal(index.lastTotal(), 0, "Last total is initially zero");
 
-        tmp = peerIndex.add(1);
+        tmp = index.add(5);
         equal(tmp, 1, "Index position");
-        deepEqual(peerIndex._weights, [5, 1], "Weights after adding 1");
-        deepEqual(peerIndex._totals, [5, 6], "Totals after adding 1");
+        deepEqual(index._weights, [0, 5], "Weights after adding 5");
+        deepEqual(index._totals, [0, 5], "Totals after adding 5");
+        equal(index.lastTotal(), 5, "Last total after adding 5");
+
+        tmp = index.add(1);
+        equal(tmp, 2, "Index position");
+        deepEqual(index._weights, [0, 5, 1], "Weights after adding 1");
+        deepEqual(index._totals, [0, 5, 6], "Totals after adding 1");
+        equal(index.lastTotal(), 6, "Last total after adding 1");
     });
 
     test("Removal", function () {
-        var peerIndex = $PeerIndex.create(),
+        var index = $Index.create(),
             lookup = {};
 
-        lookup.foo = peerIndex.add(5);
-        lookup.bar = peerIndex.add(1);
-        lookup.hello = peerIndex.add(2);
+        lookup.foo = index.add(5);
+        lookup.bar = index.add(1);
+        lookup.hello = index.add(2);
 
-        deepEqual(peerIndex._empties, {}, "Registry of empties before removal");
+        deepEqual(index._empties, {}, "Registry of empties before removal");
 
-        peerIndex.remove(lookup.foo);
+        index.remove(lookup.foo);
 
-        deepEqual(peerIndex._weights, [5, 1, 2], "Weights index unchanged after removal");
-        deepEqual(peerIndex._totals, [5, 6, 8], "Totals index unchanged after removal");
+        deepEqual(index._weights, [0, 5, 1, 2], "Weights index unchanged after removal");
+        deepEqual(index._totals, [0, 5, 6, 8], "Totals index unchanged after removal");
         deepEqual(
-            peerIndex._empties,
+            index._empties,
             {
-                5: {0: true}
+                5: {1: true}
             },
             "Registry of empties after removal"
         );
+        equal(index.emptyCount, 1, "Empty count after removal");
 
-        peerIndex.remove(lookup.hello);
+        index.remove(lookup.hello);
         deepEqual(
-            peerIndex._empties,
+            index._empties,
             {
-                5: {0: true},
-                2: {2: true}
+                5: {1: true},
+                2: {3: true}
             },
             "Registry of empties after removal"
         );
+        equal(index.emptyCount, 2, "Empty count after removal");
+    });
+
+    test("Querying", function () {
+        var index = $Index.create();
+        index.add(5);
+        index.add(1);
+        index.add(2);
+
+        equal(index.get(4), 0, "Position of 4 (inexact)");
+        equal(index.get(6), 2, "Position of 6 (exact)");
+        equal(index.get(8), 3, "Position of 8 (upper extreme, exact)");
     });
 }(
     prime.PeerIndex,
