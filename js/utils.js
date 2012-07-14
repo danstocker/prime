dcacd5f3a708cf6c7297088c6ea88a841e98f3fa
Dan Stocker <dan@kwaia.com>
2012-07-14 15:33:08 +0200

    obsolete utility functions removed

--- js/utils.js
+++ js/utils.js
@@ -38,58 +38,6 @@ troop.promise(prime, 'utils', function () {
                     }
                 }
                 return undefined;
-            },
-
-            /**
-             * Sets a value on the path specified by argument list.
-             * Last param is the value.
-             * @param object {object} Root object.
-             */
-            set: function (object) {
-                var tmp = object,
-                    length = arguments.length - 2,
-                    i, key;
-                for (i = 1; i < length; i++) {
-                    key = arguments[i];
-                    if (typeof tmp[key] !== 'object') {
-                        tmp[key] = {};
-                    }
-                    tmp = tmp[key];
-                }
-                tmp[arguments[i]] = arguments[i + 1];
-            },
-
-            /**
-             * Deletes value on path with all its parents that remain empty.
-             * Path is specified by argument list.
-             * @param object {object} Root object.
-             * @returns {boolean} Whether object is empty after removal.
-             */
-            unset: function (object) {
-                var path = arguments[1],
-                    key;
-
-                if (!(path instanceof Array)) {
-                    // taking arguments and forming a path from it
-                    path = Array.prototype.slice.call(arguments);
-                    path.shift();
-                }
-
-                // taking next key from path
-                key = path.shift();
-
-                if (object.hasOwnProperty(key)) {
-                    // when we reached the end of the path
-                    // or have an empty node
-                    if (path.length === 0 ||
-                        this.unset(object[key], path)
-                        ) {
-                        // removing
-                        delete object[key];
-                    }
-                }
-
-                return this.isEmpty(object);
             }
         });
 });
--- js/utils.test.js
+++ js/utils.test.js
@@ -10,59 +10,6 @@
         equal(typeof $utils.firstProperty({}), 'undefined', "First available key in empty object");
         equal(typeof $utils.firstProperty(1), 'undefined', "First available key in invalid object");
     });
-
-    test("Modifications", function () {
-        var tmp = {};
-        $utils.set(tmp, 'hello', 'dolly');
-        deepEqual(
-            tmp,
-            {hello: 'dolly'},
-            "Setting string value on non-existing path"
-        );
-
-        $utils.set(tmp, 'hello', 'my', {dear: 'dolly'});
-        deepEqual(
-            tmp,
-            {
-                hello: {
-                    my: {
-                        dear: 'dolly'
-                    }
-                }
-            },
-            "Setting object value on non-existing path"
-        );
-
-        $utils.set(tmp, 'hello', 'little', 'pony');
-        deepEqual(
-            tmp,
-            {
-                hello: {
-                    my: {
-                        dear: 'dolly'
-                    },
-                    little: 'pony'
-                }
-            },
-            "Setting additional value on partially existing path"
-        );
-
-        equal($utils.unset(tmp, 'hello', 'boo'), false, "Attempting to unset invalid path");
-
-        $utils.unset(tmp, 'hello', 'my', 'dear');
-        deepEqual(
-            tmp,
-            {
-                hello: {
-                    little: 'pony'
-                }
-            },
-            "Object removed and cleaned up"
-        );
-
-        $utils.unset(tmp, 'hello', 'little');
-        deepEqual(tmp, {}, "Object completely emptied");
-    });
 }(
     prime.utils
 ));
