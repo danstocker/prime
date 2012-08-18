9a3d345906ee3afad66966a5d2622c71dfdb71eb
Dan Stocker <dan@kwaia.com>
2012-08-19 00:34:55 +0200

    troop upgraded to 0.1.6

--- js/prime.js
+++ js/prime.js
@@ -8,5 +8,5 @@ var prime = {},
 
 // adding Node.js dependencies
 if (typeof exports === 'object' && typeof require === 'function') {
-    troop = require('troop-latest').troop;
+    troop = require('troop-0.1.6').troop;
 }
