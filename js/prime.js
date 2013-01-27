a0bae0b0616c783f3f07ac205e70575ed3d15982
Dan Stocker <dan@kwaia.com>
2013-01-27 03:53:35 +0100

    version numbers updated for node modules

--- js/prime.js
+++ js/prime.js
@@ -10,7 +10,7 @@ var prime = {},
 
 // adding Node.js dependencies
 if (typeof exports === 'object' && typeof require === 'function') {
-    dessert = require('dessert-0.2.2').dessert;
+    dessert = require('dessert-0.2.3').dessert;
     troop = require('troop-0.2.3').troop;
     sntls = require('sntls-0.1.3').sntls;
 }
