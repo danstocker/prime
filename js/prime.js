8cb6810c90569e52df75368cbcda7adda9769b6f
Dan Stocker <dan@kwaia.com>
2013-01-27 01:06:25 +0100

    sntls added to Node dependencies

--- js/prime.js
+++ js/prime.js
@@ -5,10 +5,12 @@
 /** @namespace */
 var prime = {},
     dessert,
-    troop;
+    troop,
+    sntls;
 
 // adding Node.js dependencies
 if (typeof exports === 'object' && typeof require === 'function') {
     dessert = require('dessert-0.2.2').dessert;
     troop = require('troop-0.2.3').troop;
+    sntls = require('sntls-0.1.3').sntls;
 }
